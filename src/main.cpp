bac8cf2657ed5b4932120ae14fa1f62b1f9cd0e5
akira215 <akira215corp@gmail.com>
2022-12-24 11:40:49 -0500

    Testing OpenXLSX lib

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include <OpenXLSX.hpp>
+
+using namespace OpenXLSX;
+
+int main() {
+
+    XLDocument doc;
+    doc.open("./CTest.xlsx");
+    auto wks = doc.workbook().worksheet("Input");
+
+
+    auto rng = wks.range(XLCellReference("A1"), XLCellReference("C20"));
+    XLDefinedName testrg = doc.workbook().definedName("defined_in_sheet");
+    //doc.workbook().definedName("defined_in_shee");
+
+    testrg = doc.workbook().definedName("testRange");
+    XLCellValue iteration;
+    for (auto& cell : testrg) 
+        iteration = cell.value();
+
+
+    XLCell i = testrg.firstCell();
+    //XLCellValue i = testrg.firstValue();
+    XLCellValue itet = i.value();
+    i.value() = "Changed By ASH";
+    std::string n = testrg.name();
+    //doc.workbook().definedName("nbAngles");
+
+    //wks.cell("A1").value() = "Hello, OpenXLSX ASHHHH!";
+
+    doc.save();
+    doc.close();
+
+    return 0;
+}
