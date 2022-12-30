3ce9406966473cb5afd988560a510e1ae69a2988
akira215 <akira215corp@gmail.com>
2022-12-29 19:54:07 -0500

    update testing

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,12 @@ using namespace OpenXLSX;
 int main() {
 
     XLDocument doc;
-    doc.open("./tablecrea.xlsx");
-    auto wks = doc.workbook().worksheet("Matrix");
+    doc.open("./Testcrea.xlsx");
+    //auto wks = doc.workbook().worksheet("Matrix");
 
 
-    doc.workbook().addTable("Matrix","Tableau2","B1:C20");
-
-    auto matrix = wks.range("C4:G16");
-    int myCell = matrix[57].value();
-
-
-    auto testTable = doc.workbook().table("tbl_one");
+    doc.workbook().addTable("Matrix","Tableau2","J10:L16");
+    //doc.workbook().addWorksheet("testCpp");
 
 
 
