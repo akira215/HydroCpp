2e00ea776122685372efe6bed75b06fa2ccc7bb5
akira215 <akira215corp@gmail.com>
2023-01-02 23:52:17 +0100

    update testing

--- src/main.cpp
+++ src/main.cpp
@@ -6,35 +6,13 @@ using namespace OpenXLSX;
 
 int main() {
 
-    
-    // First, create a new document and access the 1st sheet.
-    cout << "\nGenerating spreadsheet ..." << endl;
     XLDocument doc;
-    doc.create("./HydroCpp.xlsx");
-    auto wks = doc.workbook().sheet(1).get<XLWorksheet>();
-    string sheetName = wks.name();
-
-    // Fill the column header and create the table
-    // If the table header is not set, default name will be setup
-    // Also columns name shall be unique, a increment marks is added if required
-    cout << "Creating table ..." << endl;
-    wks.cell("B2").value() = "#";
-    wks.cell("C2").value() = "One";
-    wks.cell("D2").value() = "Col";
-    wks.cell("E2").value() = "With";
-    wks.cell("F2").value() = "Table";
-
-    XLTable myTable = doc.workbook().addTable(sheetName,"MyTable","B2:F28");
-
-    // Save the sheet...
-    cout << "Saving spreadsheet ..." << endl;
-    doc.save();
-    doc.close();
-
-    // ...and reopen it (just to make sure that it is a valid .xlsx file)
+    
+        // ...and reopen it (just to make sure that it is a valid .xlsx file)
     cout << "Re-opening spreadsheet ..." << endl << endl;
-    doc.open("./HydroCpp.xlsx");
-    wks = doc.workbook().worksheet(sheetName);
+    doc.open("./Demo10.xlsx");
+    auto wks = doc.workbook().sheet(1).get<XLWorksheet>();
+    //wks = doc.workbook().worksheet(sheetName);
 
     // We can retrieve the headers name:
     XLTable tbl = doc.workbook().table("MyTable");
@@ -56,32 +34,24 @@ int main() {
     auto nCol       = tbl.columnsCount();    // Only data body table range (excl. total & headers)
     
     int j = 0;
-/*
-    for (auto row = tbl.tableRows().begin(); row != tbl.tableRows().end(); ++row){
-        (*row)[tbl.columnIndex("#")].value() = j;
-        (*row)[1].value() = "Data" + to_string(j);
-        (*row)[tbl.columnIndex("Table")].value() = (float)j * 2.0f / 3.0f;
-        j++;
-
-    }
-
-*/
-    for(auto& row : tbl.tableRows()){
+    for(const auto& row : tbl.tableRows()){
         row[tbl.columnIndex("#")].value() = j;
         row[1].value() = "Data" + to_string(j);
         row[tbl.columnIndex("Table")].value() = (float)j * 2.0f / 3.0f;
         j++;
     }
 
-
+    // Also show the total with selected function
     tbl.autofilter().hideArrows();
     //tbl.setHeaderVisible(false);
     tbl.setTotalVisible(true);
     //tbl.column("Table")->setTotalsRowFunction("sum");
-    //tbl.column("Table")->setTotalsRowFunction("count");
+    tbl.column("Table").setTotalsRowFunction("");
     tbl.column("Table").setTotalsRowFunction("count");
-    
 
+    cout << "Table Style : " << tbl.tableStyle().style() << endl;
+    tbl.tableStyle().setStyle("test"); 
+    
 
     doc.save();
     doc.close();
